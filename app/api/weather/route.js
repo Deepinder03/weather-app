64dd74cfd663b6c3cf2fe7f3b41b8fd492373f8b
Deepinder03 <deepinderkumar81@gmail.com>
2024-04-22 19:57:43 -0700

    weather

--- app/api/weather/route.js
+++ app/api/weather/route.js
@@ -4,25 +4,32 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(request) {
   const { searchParams } = new URL(request.url);
   const address = searchParams.get("address");
-  
-  // Ensure there is an address to query the weather data
-  if (!address) {
-    return new NextResponse(JSON.stringify({ error: "Address parameter is missing" }), {
+  const latitude = searchParams.get("lat");
+  const longitude = searchParams.get("lon");
+
+  let url;
+
+  // Decide the API request format based on the provided parameters
+  if (address) {
+    url = `https://api.openweathermap.org/data/2.5/weather?q=${address}&appid=c02ceeecd4e685ea17f27d6e09d41e0f`;
+  } else if (latitude && longitude) {
+    url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=c02ceeecd4e685ea17f27d6e09d41e0f`;
+  } else {
+    // Return error if neither an address nor coordinates are provided
+    return new NextResponse(JSON.stringify({ error: "Address or latitude and longitude parameters are missing" }), {
       status: 400,
       headers: {
         'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*' // Allows all domains, adjust in production
+        'Access-Control-Allow-Origin': '*'
       }
     });
   }
-
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${address}&appid=c02ceeecd4e685ea17f27d6e09d41e0f`;
 
   console.log("Fetching URL:", url);
   const res = await fetch(url);
 
   const data = await res.json();
-  
+
   // Create a response object and modify its headers to include CORS headers
   const response = NextResponse.json({ data });
   response.headers.set('Access-Control-Allow-Origin', '*'); // Allows all domains
