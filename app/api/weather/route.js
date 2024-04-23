d7ba131a4752fb33d400bbca4a7ff7a8349b1f24
Deepinder03 <deepinderkumar81@gmail.com>
2024-04-22 17:28:37 -0700

    weather

--- app/api/weather/route.js
+++ app/api/weather/route.js
@@ -6,8 +6,15 @@ export async function GET(request) {
   const address = searchParams.get("address");
   const latitude = searchParams.get("lat");
   const longitude = searchParams.get("lon");
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${address}&appid=77d0643bee0f2abb0cd8cc6e595678ca`;
-  
+  let url = "https://api.openweathermap.org/data/2.5/weather?q=" +  address +  "&appid=" +
+  "c02ceeecd4e685ea17f27d6e09d41e0f";
+  if (address) {
+    url =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      address +
+      "&appid=" +
+      "c02ceeecd4e685ea17f27d6e09d41e0f";
+  } 
   console.log(url);
   const res = await fetch(url);
 
